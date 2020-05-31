4f478c961af9f8ca9fbc56853dcf2c3d2920b1b4
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2020-05-31 17:12:52 -0300

    organização e semântica.

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ DATAFRAME_MOVIES = pd.read_csv("movies.csv", usecols=["movieId", "title"], sep="
 print(DATAFRAME_MOVIES)
 
 username = input("\nNome de Usuário:")
-id_filme = int(input("ID do Filme:"))
+idFilme = int(input("ID do Filme:"))
 
 tags   = []
 filmes = []
@@ -15,7 +15,7 @@ filme = DATAFRAME["movieId"].values
 tag   = DATAFRAME["tag"].values
 
 for i in range(len(DATAFRAME["movieId"].values)):
-    if id_filme == filme[i]:
+    if idFilme == filme[i]:
         if tag[i] not in tags:
             tags.append(tag[i].lower())
 
@@ -24,24 +24,24 @@ for j in range(len(DATAFRAME["tag"].values)):
         if filme[j] not in filmes:
             filmes.append(filme[j])
 
-filmes_id       = DATAFRAME_MOVIES["movieId"]
-filmes_nome     = DATAFRAME_MOVIES["title"]
-filme_escolhido = ""
+filmesId       = DATAFRAME_MOVIES["movieId"].values
+filmesNome     = DATAFRAME_MOVIES["title"].values
+filmeEscolhido = ""
 
-filmes_recomendados = []
+filmesRecomendados = []
 
 for k in range(len(DATAFRAME_MOVIES["title"].values)):
-    if filmes_id[k] == id_filme:
-        filme_escolhido = filmes_nome[k]
+    if filmesId[k] == idFilme:
+        filmeEscolhido = filmesNome[k]
 
 for l in range(len(filmes)):
-    for m in range(len(DATAFRAME_MOVIES["title"])):
-        if filmes[l] == filmes_id[m]:
-            filmes_recomendados.append(filmes_nome[m])
+    for m in range(len(DATAFRAME_MOVIES["title"].values)):
+        if filmes[l] == filmesId[m]:
+            filmesRecomendados.append(filmesNome[m])
 
 print("\nUsuário:", username
-      + "\nFilme:", "[" + str(id_filme) + "]", filme_escolhido)
+      + "\nFilme:", "[" + str(idFilme) + "]", filmeEscolhido)
 print("Tags:", str(tags).strip("[]")
       + "\nFilmes Recomendados:\n",
       str(filmes)+"\n"+
-      str(filmes_recomendados).strip("[]"))
+      str(filmesRecomendados).strip("[]"))
